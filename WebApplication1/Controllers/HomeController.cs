cc6fd9ac5b1410efa010535c4b4134cb95baaa97
edemaere <demaere.ezra@gmail.com>
2023-04-11 13:58:31 -0600

    added sexy filter ;)

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using IntexWinter.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApplication1.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplication1.Controllers
 {
@@ -29,31 +30,61 @@ namespace WebApplication1.Controllers
             return View();
         }
 
-        public IActionResult Burial_Records(int pageNum = 1)
+        [Authorize(AuthenticationSchemes = "Identity.Application")]
+        public IActionResult Burial_Records(int? id, string burialSex, int pageNum = 1)
         {
             int pageSize = 10;
 
+            //Create new IQueryable so we can change display data without changing actual repo
+            var BurialsIQeryable = repo.Burialmains;
+            foreach (Burialmain burial in BurialsIQeryable)
+            {
+                //Change burial.Sex of BurialsIQeryable for display
+                if (burial.Sex == null || burial.Sex == "")
+                {
+                    burial.Sex = "N/A";
+                } else if (burial.Sex == "M")
+                {
+                    burial.Sex = "Male";
+                } else if (burial.Sex == "F")
+                {
+                    burial.Sex = "Female";
+                }
+            }
+
             var x = new BurialsViewModel
             {
-                Burialmains = repo.Burialmains
-                .OrderBy(b => b.Burialid)
-                .Skip((pageNum - 1) * pageSize)
-                .Take(pageSize),
 
-                PageInfo = new PageInfo
-                {
-                    TotalNumBurials = repo.Burialmains.Count(),
-                        //(bookCategory == null
-                        //? repo.Burialmains.Count()
-                        //: repo.Burialmains.Where(x => x.Category == bookCategory).Count()),
-                    BurialsPerPage = pageSize,
-                    CurrentPage = pageNum
-                }
-            };
+            Burialmains = BurialsIQeryable
+            //.Include(x=>x.burialmainTextiles).ThenInclude(y=>y.Textile).SingleOrDefault(m=>m.Id == id)
+            .Where(b => b.Sex == burialSex || burialSex == null)
+            .OrderBy(b => b.Burialid)
+            .Skip((pageNum - 1) * pageSize)
+            .Take(pageSize),
+
+            //Update pageNum and pageSize to show correct number of page tabs
+            PageInfo = new PageInfo
+            {
+                TotalNumBurials =
+                    (burialSex == null
+                    ? BurialsIQeryable.Count()
+                    : BurialsIQeryable.Where(x => x.Sex == burialSex).Count()),
+                BurialsPerPage = pageSize,
+                CurrentPage = pageNum
+            }
+        };
+
             return View(x);
         }
 
 
+        [Authorize(AuthenticationSchemes = "Identity.Application")]
+        public IActionResult Burial_Details()
+        {
+            return View();
+        }
+
+        [Authorize(AuthenticationSchemes = "Identity.Application")]
         public IActionResult Sex_Analysis()
         {
             return View();
