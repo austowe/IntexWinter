b9ca0de1b3dfa06b68fe0cc9766b7c22d073af6d
CameronPike <cp475@byu.edu>
2023-04-12 14:57:19 -0600

    Some Crud. Edit currently not working

--- WebApplication1/Models/IIntexWinterRepository.cs
+++ WebApplication1/Models/IIntexWinterRepository.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
 
 namespace IntexWinter.Models
 {
     public interface IIntexWinterRepository
     {
         IQueryable<Burialmain> Burialmains { get; }
+        Burialmain GetById(long id);
+        void Update(Burialmain burial);
+        void Add(Burialmain burial);
+        void Delete(Burialmain burial);
+        void SaveChanges();
     }
 }
---
+++ WebApplication1/Models/ViewModels/BurialEditViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntexWinter.Models.ViewModels
+{
+    public class BurialEditViewModel
+    {
+        public Burialmain BurialMain { get; set; }
+        public long Id { get; set; }
+        public string SquareNorthSouth { get; set; }
+        public string HeadDirection { get; set; }
+        public string Sex { get; set; }
+        public string NorthSouth { get; set; }
+        public string Depth { get; set; }
+        public string EastWest { get; set; }
+        public string AdultSubadult { get; set; }
+        public string FaceBundles { get; set; }
+        public string SouthToHead { get; set; }
+        public string Preservation { get; set; }
+        public string FieldBookPage { get; set; }
+        public string SquareEastWest { get; set; }
+        public string Goods { get; set; }
+        public string Text { get; set; }
+        public string Wrapping { get; set; }
+        public string HairColor { get; set; }
+        public string WestToHead { get; set; }
+        public string SamplesCollected { get; set; }
+        public long? Burialid { get; set; }
+        public string Area { get; set; }
+        public string Length { get; set; }
+        public string BurialNumber { get; set; }
+        public string DataExpertInitials { get; set; }
+        public string WestToFeet { get; set; }
+        public string AgeAtDeath { get; set; }
+        public string SouthToFeet { get; set; }
+        public string ExcavationRecorder { get; set; }
+        public string Photos { get; set; }
+        public string Hair { get; set; }
+        public string BurialMaterials { get; set; }
+        public DateTime? DateOfExcavation { get; set; }
+        public string FieldBookExcavationYear { get; set; }
+        public string ClusterNumber { get; set; }
+        public string ShaftNumber { get; set; }
+    }
+}
